2b51fefe04cffae79d77ca0e176a33ade001b448
chen <1279262533@qq.com>
2017-07-04 16:43:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/cdgame/mrpg/object/BaseObject.java
@@ -0,0 +1,12 @@
+package cn.com.cdgame.mrpg.object;
+
+/**
+ * 万物始于此
+ * 上帝说，要有光，于是有了这个类（误）
+ */
+
+public class BaseObject {
+
+    String name;
+
+}
---
+++ app/src/main/java/cn/com/cdgame/mrpg/object/role/BaseRole.java
@@ -0,0 +1,16 @@
+package cn.com.cdgame.mrpg.object.role;
+
+import cn.com.cdgame.mrpg.object.BaseObject;
+
+/**
+ * 角色类
+ *
+ */
+public class BaseRole extends BaseObject {
+
+    String gender ; //性别
+    String age; //年龄
+  
+
+
+}
